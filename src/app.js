4361ff8536837371f459f7a5e7e75c15585a215d
Deepak Ch. Kalita <deepakkalita102@gmail.com>
2025-09-07 12:37:29 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -8,17 +8,26 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
+
 app.use(express.json({
     limit: "16kb"
 }))
 app.use(express.urlencoded({
     extended: true,
-    linit: "16kb"
+    limit: "16kb"
 })) 
 app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
+import userRouter from "./routes/user.routes.js"
 
 
-export { express }
+
+//route declarations
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js"
 
 dotenv.config({
     path: "./.env"
@@ -17,7 +18,7 @@ connectDB()
         console.log(`Server is running at port: ${process.env.PORT}`);
     })
 })
-.catch(err, () => {
+.catch((err) => {
     console.log("MONGODB CONNECTION ERROR: ", err);
     
 })
