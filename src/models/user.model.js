afa2d7acaa8932016fae5d138fef1efe4ecd19d7
Deepak Ch. Kalita <deepakkalita102@gmail.com>
2025-09-11 20:38:18 +0530

    implement RefreshAccessToken functionality and update user routes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,7 +54,6 @@ userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next()
 
     this.password = await bcrypt.hash(this.password, 10)
-    this.password = await bcrypt.hash(this.password, 10)
     next()
 })  
 
